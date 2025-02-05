75ad1a5357e12a3e3e5171015eefb153ca5655ae
Sami <sami.daraghmeh1306@gmail.com>
2025-02-05 16:42:21 +0200

    some edits

--- MediTrackProject/client/src/Components/CreateUsers.jsx
+++ MediTrackProject/client/src/Components/CreateUsers.jsx
@@ -11,10 +11,10 @@ const CreateUsers = () => {
         <>
             <AdminBar path={path} />
             <form action="#" className="login-form">
-                <div className="login"> 
+                <div className="login" style={{marginTop:"10%"}}> 
                 <Link className="back-link" to={`/${path.charAt(0) + path.slice(1)}`}>{"<"} Back to {path.charAt(0).toUpperCase() + path.slice(1)} Table</Link>
                     <h1>Create {path.charAt(0).toUpperCase() + path.slice(1)}</h1> 
-                    <div className="inputs">
+                    <div className="inputs" >
                         <div className="firstName">
                             <input type="text" placeholder="First name..." className="input-field"/><br />
                         </div>
@@ -25,11 +25,15 @@ const CreateUsers = () => {
                             <input type="email" placeholder="Email..." className="input-field"/>
                         </div>
                         <div className="password">
+                            <input type="number" placeholder="Phone Number..." className="input-field"/>
+                        </div>
+                        <div className="password">
                             <input type="password" placeholder="Password..." className="input-field"/>
                         </div>
                         <div className="password">
                             <input type="password" placeholder="Confirm password..." className="input-field"/>
                         </div>
+                        <input type="hidden" />
                     </div>
                     <button className="button-19" role="button">Submit</button>
                 </div>
